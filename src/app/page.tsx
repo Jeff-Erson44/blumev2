7b615ca3735ac512e7a2fe08b144a4b73429adc1
Jeff Erson <74770087+Jeff-Erson44@users.noreply.github.com>
2024-10-26 21:06:26 +0200

    fix error deployement

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import { SoluCard } from "./components/assets/SoluCard/SoluCard";
 import { Banner } from "./components/Banner/Banner";
 import { Hero } from "./components/Hero/Hero";
 import { LatestNews } from "./components/LatestNews/LatestNews";
