9575d6149f3b8e3c47d19ba5ae16d52333be160e
Jeff Erson <74770087+Jeff-Erson44@users.noreply.github.com>
2024-10-28 19:18:24 +0100

    fix deployement

--- src/app/components/Garanti/Garanti.tsx
+++ src/app/components/Garanti/Garanti.tsx
@@ -5,10 +5,10 @@ import Image from 'next/image';
 export const Garanti= () => {
     return(
         <section className='containerGaranti'>
-            <h1>La garantie d'authenticité et de sécurité</h1>
+            <h1>La garantie d&apos;authenticité et de sécurité</h1>
             <div className='containerGaranti__highlight'>
                 <div className='containerGaranti__highlight__text'>
-                    <p>Chaque produit est inscrit sur notre blockchain, créant un passeport digital infalsifiable avec toutes ses informations essentielles. Une puce NFC, intégrée discrètement, est reliée à ce certificat pour une vérification simple via smartphone. Les données, cryptées et protégées sur la blockchain, assurent une traçabilité inaltérable et une sécurité optimale. Avec Blume, chaque produit de luxe de seconde main est authentique, traçable et sécurisé, garantissant une expérience d'achat et de vente unique.</p>
+                    <p>Chaque produit est inscrit sur notre blockchain, créant un passeport digital infalsifiable avec toutes ses informations essentielles. Une puce NFC, intégrée discrètement, est reliée à ce certificat pour une vérification simple via smartphone. Les données, cryptées et protégées sur la blockchain, assurent une traçabilité inaltérable et une sécurité optimale. Avec Blume, chaque produit de luxe de seconde main est authentique, traçable et sécurisé, garantissant une expérience d&apos;achat et de vente unique.</p>
                     <Button 
                         text="Vérifier l'authenticité"
                     />
